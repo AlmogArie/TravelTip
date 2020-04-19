5ec0740ca4767b1d84f31cc6c09112a6fbd9157f
AlmogArie <almogrie@gmail.com>
2020-04-19 17:40:14 +0300

    fucked up version

--- js/main.js
+++ js/main.js
@@ -1,30 +1,92 @@
-console.log('main');
+// initMap();
 
+// function initMap() {
+//     // The location of Uluru
+//     var uluru = {lat: -25.344, lng: 131.036};
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('map'), {zoom: 4, center: uluru});
+//     // The marker, positioned at Uluru
+//     var marker = new google.maps.Marker({position: uluru, map: map});
+//   }
+// var gLocations = [];
 
-initMap();
+// function getNewLocation() {
+//     let newLocation = document.querySelector('.search-location').value;
+//     onAddLocation(gLocations);
+//     renderLocationTitle(newLocation);
+// }
 
-function initMap() {
-    // The location of Uluru
-    var uluru = {lat: -25.344, lng: 131.036};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  }
-var gLocations = [];
+// function renderLocationTitle(txt) {
+//     let title = document.querySelector('.location');
+//     return title.innerText = txt;
+// }
 
-function getNewLocation() {
-    let newLocation = document.querySelector('.search-location').value;
-    onAddLocation(gLocations);
-    renderLocationTitle(newLocation);
-}
+// function onAddLocation(location){
+//   return  gLocations.push(location);
+// }
 
-function renderLocationTitle(txt) {
-    let title = document.querySelector('.location');
-    return title.innerText = txt;
-}
+function initAutocomplete() {
+    var map = new google.maps.Map(document.querySelector('.map'), {
+        center: {
+            lat: -33.8688,
+            lng: 151.2195
+        },
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
 
-function onAddLocation(location){
-  return  gLocations.push(location);
+    var input = document.querySelector('.search-location');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 }
