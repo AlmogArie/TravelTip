6e6e90a3b2deb40882729cc0b2fdac7bddca5cb9
AlmogArie <almogrie@gmail.com>
2020-04-19 16:31:27 +0300

    walla lo yodea

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('main');
+var gLocations = [];
+
+function getNewLocation() {
+    let newLocation = document.querySelector('.search-location').value;
+    onAddLocation(gLocations);
+    renderLocationTitle(newLocation);
+}
+
+function renderLocationTitle(txt) {
+    let title = document.querySelector('.location');
+    return title.innerText = txt;
+}
+
+function onAddLocation(location){
+  return  gLocations.push(location);
+}
